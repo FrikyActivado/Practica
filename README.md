b52570a6d06b139e58beea392a76c03ce44be708
FrikyActivado <brelgomo@gmail.com>
2021-04-27 10:33:25 -0600

    esto no sincroniza

--- README.md
+++
@@ -1,2 +0,0 @@
-# WebNoticiasGDS
-Una app web pwa en la cual se comunican noticias a los trabajadores de la compañia
--- js/noticias.js
+++
@@ -1,5 +0,0 @@
-function cerrarSesion(){
-	sessionStorage.removeItem("usuario");
-	alert("Sesion finalizada");
-	window.location.href="index.html";
-}
